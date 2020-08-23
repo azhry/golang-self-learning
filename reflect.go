2cad9dc90a84c05d2ebd8335038fe5636cf85c20
Azhary Arliansyah <arliansyah_azhary@yahoo.com>
2020-08-23 17:02:51 +0700

    Update reflect.go

--- reflect.go
+++ reflect.go
@@ -5,12 +5,17 @@ import "reflect"
 import "strings"
 
 func main() {
-    var filter struct {
+    type Filter struct {
         CategoryID int
         Condition string `condition:"WHERE"`
+    }
+    filter := Filter{
+        CategoryID: 1,
+        Condition: "weekend",
     }
 
     reflects := reflect.ValueOf(&filter).Elem()
     condTag := reflects.Type().Field(1).Tag.Get("condition")
     fmt.Println(strings.Split(condTag, ","))
+    fmt.Println(reflects.Field(1).Interface())
 }
